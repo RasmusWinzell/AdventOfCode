606a1112c88ca81c30448601e7665ced61a432c6
Rasmus Winzell <raswi727@student.liu.se>
2023-12-04 15:24:17 +0100

    Updated template

--- util/template.py
+++ util/template.py
@@ -14,23 +14,34 @@ def partB(input):
 
 if __name__ == "__main__":
     puzzle = Puzzle(year="#year", day="#day")
+    for example in puzzle.examples:
+        if example.answer_a:
+            answer = partA(example.input_data)
+            assert (
+                str(answer) == example.answer_a
+            ), f"Part A: Expected {example.answer_a}, got {answer}"
 
-    example = puzzle.examples[0]
-    puzzle_input = example.input_data
+    if puzzle.answered_a:
+        answer = partA(puzzle.input_data)
+        assert (
+            str(answer) == puzzle.answer_a
+        ), f"Part A: Expected {puzzle.answer_a}, got {answer}"
+    else:
+        puzzle.answer_a = partA(puzzle.input_data)
+        assert puzzle.answered_a, "Answer A not correct"
 
-    # puzzle_input = puzzle.input_data
+    for example in puzzle.examples:
+        if example.answer_b:
+            answer = partB(example.input_data)
+            assert (
+                str(answer) == example.answer_b
+            ), f"Part B: Expected {example.answer_b}, got {answer}"
 
-    # puzzle_input = """input"""
-
-    res = partA(puzzle_input)
-
-    puzzle_answer = example.answer_a
-    # puzzle_answer = example.answer_b
-    # puzzle_answer = answer
-
-    assert str(res) == str(
-        puzzle_answer
-    ), f"Part A: Expected {puzzle_answer}, got {res}"
-
-    # puzzle.answer_a = resA
-    # puzzle.answer_b = res
+    if puzzle.answered_b:
+        answer = partB(puzzle.input_data)
+        assert (
+            str(answer) == puzzle.answer_b
+        ), f"Part B: Expected {puzzle.answer_b}, got {answer}"
+    else:
+        puzzle.answer_b = partB(puzzle.input_data)
+        assert puzzle.answered_b, "Answer B not correct"
