8d0d6527e58c1090083c6e3e89272edaa6694317
Rasmus Winzell <raswi727@student.liu.se>
2023-12-05 19:05:47 +0100

    [clean] day5 2023

--- 2023/day05.py
+++ 2023/day05.py
@@ -1,157 +1,63 @@
 # Advent of Code 2023 Day 5, https://adventofcode.com/2023/day/5
 # https://github.com/RasmusWinzell/AdventOfCode/blob/master/2023/2023/day05.py
-# This is the original solution
+# This is the cleaned solution
 from aocd.models import Puzzle
+
+inf = float("inf")
+
+
+def parse(input: str):
+    seeds = list(map(int, input.split("\n", 1)[0].split()[1:]))
+    map_lines = [sec.split("\n")[1:] for sec in input.split("\n\n")[1:]]
+    maps = [[tuple(map(int, line.split())) for line in sec] for sec in map_lines]
+    return seeds, maps
 
 
 # Solved in 0:33:56 (Answer: 551761867)
 def partA(input):
-    lines = input.split("\n")
-    seeds = list(map(int, lines[0].split()[1:]))
-    maps = []
-    current_map = None
-    map_type = None
-    for line in lines[1:]:
-        if not line.strip():
-            continue
-        if ":" in line:
-            if map_type is not None:
-                maps.append(current_map)
-            current_map = []
-            map_type = line.split()[0]
-            print(map_type)
-        else:
-            ss, ds, l = map(int, line.split())
-            current_map.append((ss, ds, l))
-            print(ss, ds, l)
-    maps.append(current_map)
-
-    def traverse(value, i):
-        if i >= len(maps):
-            print(value)
-            return value
-        for ds, ss, l in maps[i]:
-            if ss <= value and ss + l > value:
-                res = traverse(value + ds - ss, i + 1)
-                return res
-        res = traverse(value, i + 1)
-        return res
-
-    lowest = float("inf")
+    seeds, maps = parse(input)
+    best_location = inf
     for seed in seeds:
-        print("seed", seed)
-        loc = traverse(seed, 0)
-        lowest = min(lowest, loc)
-    return lowest
+        value = seed
+        for range in maps:  # Loop through each map (ie seed-to-soil)
+            for dst, src, length in range:  # Loop through each range in map
+                if src <= value < src + length:
+                    value += dst - src
+                    break
+        best_location = min(best_location, value)
+    return best_location
 
 
 # Solved in 1:17:00 (Answer: 57451709)
 def partB(input):
-    lines = input.split("\n")
-    seeds = list(map(int, lines[0].split()[1:]))
-    maps = []
-    current_map = None
-    map_type = None
-    for line in lines[1:]:
-        if not line.strip():
-            continue
-        if ":" in line:
-            if map_type is not None:
-                maps.append(current_map)
-            current_map = []
-            map_type = line.split()[0]
-            print(map_type)
-        else:
-            ss, ds, l = map(int, line.split())
-            current_map.append((ss, ds, l))
-            print(ss, ds, l)
-    maps.append(current_map)
+    seeds, maps = parse(input)
 
-    def traverse(start, length, i):
-        if length <= 0:
-            return float("inf")
-        if i >= len(maps):
-            return start
-        lowest = float("inf")
-        tested = []
-        for ds, ss, l in maps[i]:
-            print(start, length, ss, l)
-            new_start = max(ss, start)
-            new_end = min(ss + l, start + length)
-            new_length = new_end - new_start
-            if new_length > 0:
-                print("yay", new_start, new_length)
-                res = traverse(new_start + ds - ss, new_length, i + 1)
-                lowest = min(lowest, res)
-                tested.append((new_start, new_end))
-        print(tested, "lowest", lowest)
-        tested = sorted(tested)
-        if not tested:
-            print("not tested")
-            res = traverse(start, length, i + 1)
-            lowest = min(lowest, res)
-        else:
-            ss, _ = tested[0]
-            res = traverse(start, min(length, ss - start), i + 1)
-            lowest = min(lowest, res)
-            print("lowest", lowest)
-            for i in range(len(tested) - 1):
-                ss1, se1 = tested[i]
-                ss2, se2 = tested[i + 1]
-                new_start = max(se1, start)
-                new_end = min(ss2, start + length)
-                new_length = new_end - new_start
-                if new_length > 0:
-                    res = traverse(new_start, new_length, i + 1)
-                    lowest = min(lowest, res)
-                    print("lowest", lowest)
-            _, se = tested[-1]
-            new_start = max(start, se)
-            res = traverse(new_start, min(length, length - new_start + start), i + 1)
-            lowest = min(lowest, res)
-            print("lowest", lowest)
-        print("   " * i, "lowest", i, lowest)
-        return lowest
+    prev_ranges = zip(seeds[:-1:2], seeds[1::2])  # Set seeds as initial ranges
+    for range_map in maps:
+        range_dict = {src + l: 0 for _, src, l in range_map}  # Add ends
+        range_dict.update({src: dst - src for dst, src, _ in range_map})  # Add starts
+        range_list = [(-inf, 0)] + sorted(range_dict.items()) + [(inf, 0)]
 
-    lowest = float("inf")
-    for seed_start, length in zip(seeds[0:-1:2], seeds[1::2]):
-        print("seed", seed_start, length)
-        loc = traverse(seed_start, length, 0)
-        print(loc)
-        lowest = min(lowest, loc)
-    return lowest
+        new_ranges = []
+        # Loop through ranges from previous iteration
+        for prev_start, prev_length in prev_ranges:
+            # Loop through all ranges in the map (ie seed-to-soil)
+            for (start, diff), (end, _) in zip(range_list[:-1], range_list[1:]):
+                new_start = max(prev_start, start)
+                new_end = min(prev_start + prev_length, end)
+                if (new_length := new_end - new_start) > 0:
+                    new_ranges.append((new_start + diff, new_length))
+        prev_ranges = new_ranges
+
+    return min(start for start, _ in prev_ranges)
 
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2023, day=5)
-    for example in puzzle.examples:
-        if example.answer_a:
-            answer = partA(example.input_data)
-            assert (
-                str(answer) == example.answer_a
-            ), f"Part A: Expected {example.answer_a}, got {answer}"
 
-    if puzzle.answered_a:
-        answer = partA(puzzle.input_data)
-        assert (
-            str(answer) == puzzle.answer_a
-        ), f"Part A: Expected {puzzle.answer_a}, got {answer}"
-    else:
-        puzzle.answer_a = partA(puzzle.input_data)
-        assert puzzle.answered_a, "Answer A not correct"
+    puzzle_input = puzzle.input_data
 
-    for example in puzzle.examples:
-        if example.answer_b:
-            answer = partB(example.input_data)
-            assert (
-                str(answer) == example.answer_b
-            ), f"Part B: Expected {example.answer_b}, got {answer}"
+    answer_a = partA(puzzle_input)
+    answer_b = partB(puzzle_input)
 
-    if puzzle.answered_b:
-        answer = partB(puzzle.input_data)
-        assert (
-            str(answer) == puzzle.answer_b
-        ), f"Part B: Expected {puzzle.answer_b}, got {answer}"
-    else:
-        puzzle.answer_b = partB(puzzle.input_data)
-        assert puzzle.answered_b, "Answer B not correct"
+    print(f"Answer A: {answer_a}, Answer B: {answer_b}")
