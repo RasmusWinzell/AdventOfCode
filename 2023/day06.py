7e166737c6d404acaa9e1be38fc0644bc4668161
Rasmus Winzell <raswi727@student.liu.se>
2023-12-06 07:22:26 +0100

    [clean] day6 2023

--- 2023/day06.py
+++ 2023/day06.py
@@ -1,70 +1,43 @@
 # Advent of Code 2023 Day 6, https://adventofcode.com/2023/day/6
 # https://github.com/RasmusWinzell/AdventOfCode/blob/master/2023/2023/day06.py
-# This is the original solution
+# This is the cleaned solution
+import re
+from math import ceil, floor, prod, sqrt
+
 from aocd.models import Puzzle
 
 
 # Solved in 0:10:05 (Answer: 5133600)
 def partA(input: str):
-    print(input)
-    lines = input.split("\n")
-    times = list(map(int, lines[0].split()[1:]))
-    dists = list(map(int, lines[1].split()[1:]))
-    res = 1
-    for i in range(len(times)):
-        wins = 0
-        for hold in range(times[i]):
-            dist = (times[i] - hold) * hold
-            if dist > dists[i]:
-                wins += 1
-        print(wins)
-        res *= wins
+    numbers = list(map(int, re.findall("\d+", input)))
+    half = len(numbers) // 2
+    times, dists = numbers[:half], numbers[half:]
+    # Components in the quadratic formula
+    quad_comps = [(t / 2, sqrt(-d + t**2 / 4)) for t, d in zip(times, dists)]
+    res = prod(ceil(mid + diff) - floor(mid - diff) - 1 for mid, diff in quad_comps)
     return res
 
 
 # Solved in 0:13:24 (Answer: 40651271)
 def partB(input):
-    lines = input.split("\n")
-    time = int(lines[0].split(" ", 1)[1].replace(" ", ""))
-    record_dist = int(lines[1].split(" ", 1)[1].replace(" ", ""))
-    wins = 0
-    for hold in range(time):
-        dist = (time - hold) * hold
-        if dist > record_dist:
-            wins += 1
-    return wins
+    numbers = re.findall("\d+", input)
+    half = len(numbers) // 2
+    time, dist = int("".join(numbers[:half])), int("".join(numbers[half:]))
+    # Use quadratic formula
+    mid = time / 2
+    diff = sqrt(-dist + time**2 / 4)
+    start = floor(mid - diff)
+    end = ceil(mid + diff)
+    records = end - start - 1
+    return records
 
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2023, day=6)
-    # for example in puzzle.examples:
-    #     if example.answer_a:
-    #         answer = partA(example.input_data)
-    #         assert (
-    #             str(answer) == example.answer_a
-    #         ), f"Part A: Expected {example.answer_a}, got {answer}"
 
-    if puzzle.answered_a:
-        answer = partA(puzzle.input_data)
-        assert (
-            str(answer) == puzzle.answer_a
-        ), f"Part A: Expected {puzzle.answer_a}, got {answer}"
-    else:
-        puzzle.answer_a = partA(puzzle.input_data)
-        assert puzzle.answered_a, "Answer A not correct"
+    puzzle_input = puzzle.input_data
 
-    for example in puzzle.examples:
-        if example.answer_b:
-            answer = partB(example.input_data)
-            assert (
-                str(answer) == example.answer_b
-            ), f"Part B: Expected {example.answer_b}, got {answer}"
+    answer_a = partA(puzzle_input)
+    answer_b = partB(puzzle_input)
 
-    if puzzle.answered_b:
-        answer = partB(puzzle.input_data)
-        assert (
-            str(answer) == puzzle.answer_b
-        ), f"Part B: Expected {puzzle.answer_b}, got {answer}"
-    else:
-        puzzle.answer_b = partB(puzzle.input_data)
-        assert puzzle.answered_b, "Answer B not correct"
+    print(f"Answer A: {answer_a}, Answer B: {answer_b}")
