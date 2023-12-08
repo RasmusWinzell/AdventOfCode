5110a80a11e9dd3bad113b512d0be7c313f5d3c0
Rasmus Winzell <raswi727@student.liu.se>
2023-12-08 16:32:37 +0100

    [clean] day8 2023 (2)

--- 2023/day08.py
+++ 2023/day08.py
@@ -2,7 +2,7 @@
 # https://github.com/RasmusWinzell/AdventOfCode/blob/master/2023/2023/day08.py
 # This is the cleaned solution
 import re
-from itertools import cycle
+from itertools import cycle, takewhile
 from math import lcm
 
 from aocd.models import Puzzle
@@ -11,24 +11,25 @@ fmt = re.compile(r"(.+) = \((.+), (.+)\)")
 a_fmt = re.compile(r"..A")
 
 
+def parse_input(input: str):
+    instrs = input.partition("\n")[0]
+    dirs = {k: {"L": l, "R": r} for k, l, r in fmt.findall(input)}
+    return dirs, instrs
+
+
 def steps_for(curr, dirs, instructions):
-    instrs, steps = cycle(instructions), 1
-    while (curr := dirs[curr][next(instrs)])[-1] != "Z":
-        steps += 1
-    return steps
+    gen = (curr := dirs[curr][instr] for instr in cycle(instructions))
+    return sum(1 for _ in takewhile(lambda c: c[-1] != "Z", gen)) + 1
 
 
 # Solved in 0:09:58 (Answer: 13771)
 def partA(input: str):
-    instrs = input.partition("\n")[0]
-    dirs = {k: {"L": l, "R": r} for k, l, r in fmt.findall(input)}
-    return steps_for("AAA", dirs, instrs)
+    return steps_for("AAA", *parse_input(input))
 
 
 # Solved in 1:07:32 (Answer: 13129439557681)
 def partB(input: str):
-    instrs = input.partition("\n")[0]
-    dirs = {k: {"L": l, "R": r} for k, l, r in fmt.findall(input)}
+    dirs, instrs = parse_input(input)
     return lcm(*(steps_for(curr, dirs, instrs) for curr in filter(a_fmt.match, dirs)))
 
 
