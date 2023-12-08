1ff43f7e173402ec6c14cf94b5fc86e9708dbfef
Rasmus Winzell <raswi727@student.liu.se>
2023-12-08 17:39:41 +0100

    [clean] day8 2023 (3)

--- 2023/day08.py
+++ 2023/day08.py
@@ -11,26 +11,30 @@ fmt = re.compile(r"(.+) = \((.+), (.+)\)")
 a_fmt = re.compile(r"..A")
 
 
-def parse_input(input: str):
-    instrs = input.partition("\n")[0]
+def steps(curr: str, input: str):
+    """
+    First row makes a dict of dicts like "xxx": {"L": "xxx", "R": "xxx"}.\n
+    Second row makes a generator that follows the instructions indefinitely.\n
+    Third row counts number of steps until "ZZZ" or "xxZ" is reached.
+    """
     dirs = {k: {"L": l, "R": r} for k, l, r in fmt.findall(input)}
-    return dirs, instrs
-
-
-def steps_for(curr, dirs, instructions):
-    gen = (curr := dirs[curr][instr] for instr in cycle(instructions))
+    gen = (curr := dirs[curr][instr] for instr in cycle(input.partition("\n")[0]))
     return sum(1 for _ in takewhile(lambda c: c[-1] != "Z", gen)) + 1
 
 
 # Solved in 0:09:58 (Answer: 13771)
 def partA(input: str):
-    return steps_for("AAA", *parse_input(input))
+    """Call steps to find number of steps to find "ZZZ" from "AAA"."""
+    return steps("AAA", input)
 
 
 # Solved in 1:07:32 (Answer: 13129439557681)
 def partB(input: str):
-    dirs, instrs = parse_input(input)
-    return lcm(*(steps_for(curr, dirs, instrs) for curr in filter(a_fmt.match, dirs)))
+    """
+    Find the number of steps to find "xxZ" for each "xxA".
+    TODO: Figure out why lcm works.
+    """
+    return lcm(*(steps(curr, input) for curr in set(a_fmt.findall(input))))
 
 
 if __name__ == "__main__":
